bc15fb22ea09678af79673e71c40d46a3cc1809b
Victor Avila <avilavictor1570@gmail.com>
2020-07-18 18:15:09 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,5 +15,5 @@ def age_appropriate_birthday(birthday_kids)
     if age.length < 12
       puts "Happy Birthday #{name}! you are now #{age} years old!"
     end
-     puts "you are too old for this"
+    "you are too old for this"
 end
